f915f891bd8b6f696feb91bd897f783922eaec73
liang-cto <liang-cto@boe.com.cn>
2020-03-16 17:11:32 +0800

    update minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+}
+
+fn read_file(filename: &String) -> String {
+    fs::read_to_string(filename).expect("Something");
 }
