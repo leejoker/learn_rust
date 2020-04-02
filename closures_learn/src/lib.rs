aece3c8c26e481419b8f39882a1e798a7f3844ee
liang-cto <liang-cto@boe.com.cn>
2020-04-02 13:40:39 +0800

    change closure_learn

--- closures_learn/src/lib.rs
+++ closures_learn/src/lib.rs
@@ -1,6 +1,37 @@
 use std::thread;
 use std::time::Duration;
 
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
 pub fn simulated_expensive_calculation(intensity: u32) -> u32 {
     println!("calculating slowly ...");
     thread::sleep(Duration::from_secs(2));
@@ -8,23 +39,63 @@ pub fn simulated_expensive_calculation(intensity: u32) -> u32 {
 }
 
 pub fn generate_workout(intensity: u32, random_number: u32) {
+    // let expensive_closure = |num: u32| -> u32 {
+    //     println!("calculating slowly...");
+    //     thread::sleep(Duration::from_secs(2));
+    //     num
+    // };
+
+    let mut expensive_result = Cacher::new(|num: u32| -> u32 {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
     if intensity < 25 {
-        println!(
-            "Today, do {} pushups!",
-            simulated_expensive_calculation(intensity)
-        );
-        println!(
-            "Next, do {} situps!",
-            simulated_expensive_calculation(intensity)
-        );
+        // println!("Today, do {} pushups!",expensive_closure(intensity));
+        println!("Today, do {} pushups!", expensive_result.value(intensity));
+        // println!("Next, do {} situps!", expensive_closure(intensity));
+        println!("Next, do {} situps!", expensive_result.value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
+            // println!("Today, run for {} minutes!", expensive_closure(intensity));
             println!(
                 "Today, run for {} minutes!",
-                simulated_expensive_calculation(intensity)
+                expensive_result.value(intensity)
             );
         }
     }
+
+    //闭包的类型需要保持一直
+    //expensive_closure(String::from("asdasdad"));
+}
+
+#[test]
+fn call_with_different_values() {
+    let mut c = Cacher::new(|a| a);
+
+    let v1 = c.value(1);
+    let v2 = c.value(2);
+
+    assert_eq!(v2, 2);
+}
+
+#[test]
+fn equal_with_values() {
+    let x = 4;
+    let equal_to_x = |z| z == x;
+    println!("{}", x);
+    let y = 4;
+    assert!(equal_to_x(y));
+}
+
+#[test]
+fn equal_with_move() {
+    let x = vec![1, 2, 3];
+    let equal_to_x = move |z| z == x;
+    println!("try to use x here {:?}", x);
+    let y = vec![1, 2, 3];
+    assert!(equal_to_x(y));
 }
--- closures_learn/src/main.rs
+++ closures_learn/src/main.rs
@@ -1,5 +1,19 @@
 use closures_learn;
 
 fn main() {
-    closures_learn::simulated_expensive_calculation(123);
+    //闭包
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    closures_learn::generate_workout(simulated_user_specified_value, simulated_random_number);
+
+    //迭代器
+    let v1: Vec<i32> = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
+
+    //TODO 实现链表完成迭代器实现部分
+    //TODO 完成minigrep项目通过迭代器进行性能优化
 }
